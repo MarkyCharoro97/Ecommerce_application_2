f02a6f160f401ddc10f4832f97c556bd75d743c2
Mclaughlin Fanwell Charoro <102194625+MarkyCharoro97@users.noreply.github.com>
2025-09-10 00:15:39 +0200

    Delete ecommerce_application_2 directory

--- ecommerce_application_2/ecommerce/api_urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import api_views
-
-urlpatterns = [
-    path("stores/", api_views.StoreListCreateView.as_view(), name="api-stores"),
-    path("stores/<int:store_id>/products/", api_views.ProductListCreateView.as_view(), name="api-store-products"),
-    path("products/<int:product_id>/reviews/", api_views.ReviewListView.as_view(), name="api-product-reviews"),
-]
